08beadb4e22eb10923e570281fded9b66c0bd1f6
Blue965 <robin.noe.voulgaris@gmail.com>
2026-04-11 18:48:39 -0400

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,12 +1,10 @@
 function showUserConfirm(user) {
-  const el = document.getElementById('user-confirm');
-  if (!el) return;
-
   document.getElementById('confirm-avatar').src = user.avatarUrl;
+
   document.getElementById('confirm-name').textContent =
     user.displayName + (user.displayName !== user.username ? ` (@${user.username})` : '');
 
-  document.getElementById('confirm-id').textContent = 'ID Roblox: ' + user.id;
+  document.getElementById('confirm-id').textContent = `ID Roblox: ${user.id}`;
 
-  el.classList.add('show');
+  document.getElementById('user-confirm').classList.add('show');
 }
